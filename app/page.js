3217534a21ff8cc3a08e0b41ba2fd063ce866278
vijayt-dev <vijayatk3@gmail.com>
2023-06-29 12:10:48 +0530

    add link

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/users">Go to Users</Link>
+      <Link href="/image">Go to Images</Link>
     </main>
   );
 }
